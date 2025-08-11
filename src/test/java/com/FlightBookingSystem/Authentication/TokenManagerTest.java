d1c5f0576474555dac5909ca8cd40118c2bfcba9
bharath.p <bharathbhd3@gmail.com>
2025-08-11 19:23:26 +0530

    testing TokenManager done!

--- src/test/java/com/FlightBookingSystem/Authentication/TokenManagerTest.java
+++ src/test/java/com/FlightBookingSystem/Authentication/TokenManagerTest.java
@@ -1,5 +1,6 @@
 package com.FlightBookingSystem.Authentication;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TokenManagerTest {
@@ -7,9 +8,17 @@ public class TokenManagerTest {
 	/**
 	 * @author windass
 	 * */
-	
-	@Test
-	public void testAccessToken() {
-		
-	}
+
+    @Test
+    public void testAccessToken() {
+        // Call the method to generate the token
+        String token = TokenManager.generateToken();
+
+        // Validate that the token is not null or empty
+        Assert.assertNotNull(token, "Access token should not be null");
+        Assert.assertFalse(token.isEmpty(), "Access token should not be empty");
+
+        // Optional: further validation can be done by making a request using this token
+        // to a protected resource endpoint if available, to verify token validity.
+    }
 }
