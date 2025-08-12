fb7578a77c5f9a3ed9cace1510e13fa729d296b3
bharath.p <bharathbhd3@gmail.com>
2025-08-12 12:24:44 +0530

    Reporting:Extent-Reports-Included

---
+++ src/test/java/com/FlightBookingSystem/Listeners/ExtentTestNGListener.java
@@ -0,0 +1,47 @@
+package com.FlightBookingSystem.Listeners;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ExtentTestNGListener implements ITestListener {
+
+    private static ExtentReports extent = createInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    private static ExtentReports createInstance() {
+        ExtentSparkReporter reporter = new ExtentSparkReporter("test-output/extent-report.html");
+        ExtentReports extentReports = new ExtentReports();
+        extentReports.attachReporter(reporter);
+        return extentReports;
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
